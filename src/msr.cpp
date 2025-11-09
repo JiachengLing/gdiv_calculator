2684339389e06514748c4b4ff855dd20557a16e0
jcling0107907060 <jcling0107907060@gmail.com>
2025-11-09 21:25:25 +0800

    Update msr.cpp

--- src/msr.cpp
+++ src/msr.cpp
@@ -1,21 +1,32 @@
 #include "gdiv_utils.h"
 
 int msr_compute(const char* path, const RasterOptions* opt,
-                double* mean, double* stdv, double* vmin, double* vmax, uint64_t* valid) {
-    long double sum=0, sumsq=0;
-    double mn=std::numeric_limits<double>::infinity(), mx=-mn;
-    uint64_t n=0;
+                double* mean, double* var, double* vmin, double* vmax, uint64_t* valid) {
+    long double sum = 0, sumsq = 0;
+    double mn = std::numeric_limits<double>::infinity();
+    double mx = -mn;
+    uint64_t n = 0;
 
+    // Loop through all raster pixels (helper from gdiv_utils)
     int rc = for_each_pixel_double(path, opt, [&](double v){
-        sum += v; sumsq += (long double)v*v; ++n;
-        if (v<mn) mn=v; if (v>mx) mx=v;
+        sum += v;
+        sumsq += (long double)v * v;
+        ++n;
+        if (v < mn) mn = v;
+        if (v > mx) mx = v;
     }, /*require_non_empty=*/true);
     if (rc) return rc;
 
-    *mean = (double)(sum/n);
-    long double var = (sumsq/n) - ((long double)(*mean)*(*mean));
-    if (var < 0) var = 0;
-    *stdv = std::sqrt((double)var);
-    *vmin = mn; *vmax = mx; *valid = n;
+    *mean = (double)(sum / n);
+
+    // Variance = E[x^2] - (E[x])^2
+    long double variance = (sumsq / n) - ((long double)(*mean) * (*mean));
+    if (variance < 0) variance = 0;
+
+    *var = (double)variance;
+    *vmin = mn;
+    *vmax = mx;
+    *valid = n;
+
     return 0;
 }
